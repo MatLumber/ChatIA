e1ed69be368f227d37c7ea84dd5fe649ca1d612e
MatLumber <89222794+MatLumber@users.noreply.github.com>
2024-10-25 20:57:33 -0300

    Commit para deploy y morir2

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,14 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCDTi7y818BdiBuSb226ey9LLJ92yfF5Po",
-    authDomain: "matai-552cc.firebaseapp.com",
-    projectId: "matai-552cc",
-    storageBucket: "matai-552cc.appspot.com",
-    messagingSenderId: "397553247396",
-    appId: "1:397553247396:web:5de2f5eeae26f281053231",
-    measurementId: "G-G4WLR80P1G"
-};
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
